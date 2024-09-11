bd7f82ed8cd21f97ba73876fcd6ff975932710d0
Mike Nguyen <mikeynguyen29@gmail.com>
2024-09-11 14:33:37 -0400

    Started on basic Java programming exercises 09/10/24

--- exercises/src/main/java/edu/nguyenmv/exercises03/Oregon_trail.java
+++ exercises/src/main/java/edu/nguyenmv/exercises03/Oregon_trail.java
@@ -4,6 +4,14 @@ public class Oregon_trail {
     public static void main(String[] args)
     {
         System.out.println("Hello, traveler!");
+
+        int totalFood = 200;
+        int numPeople = 5;
+        int ration = 2;
+
+        int totalDays =  totalFood / (numPeople*ration);
+        System.out.println("TOTAL DAYS: " + totalDays);
+        
     }
 
 
