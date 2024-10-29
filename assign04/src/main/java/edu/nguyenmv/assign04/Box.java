3230e88b0b1b085f01935c35bebfab3ed8b53b8f
Mike Nguyen <mikeynguyen29@gmail.com>
2024-10-29 14:08:02 -0400

    starting assign 04

---
+++ assign04/src/main/java/edu/nguyenmv/assign04/Box.java
@@ -0,0 +1,16 @@
+package edu.nguyenmv.assign04;
+
+public class Box {
+
+    public Box(int sr, int sc, int er, int ec, boolean filled, char drawChar) {
+
+    }
+
+    public String toString() {
+    return null;
+    }
+
+    public void draw(GameBoard s) {
+
+    }
+}
---
+++ assign04/src/main/java/edu/nguyenmv/assign04/GameBoard.java
@@ -0,0 +1,55 @@
+package edu.nguyenmv.assign04;
+
+public class GameBoard {
+private int rowCnt;
+private int colCnt;
+private char fillChar;
+private char [][] board;
+
+    public GameBoard(int rowCnt, int colCnt, char fillChar){
+        this.rowCnt = rowCnt;
+        this.colCnt = colCnt;
+        for (int i = 0; i < colCnt ; i++) {
+            for (int j = 0; j < rowCnt ; j++) {
+                board[i][j] = fillChar;
+
+            }
+        }
+        return;
+    }
+
+    public void clear() {
+
+    }
+    public int getRowCnt(int colCnt) {
+        return colCnt;
+    }
+
+    public int getColCnt(int rowCnt) {
+    return rowCnt;
+    }
+
+    public boolean isValidPosition(int row, int col) {
+        return false;
+
+    }
+
+    public char getPos(int row, int col) {
+
+    }
+
+
+    public boolean setPos(int row, int col, char c) {
+        return false;
+    }
+
+    public String toString() {
+        return null;
+
+    }
+
+    public String getBoardString() {
+        return null;
+    }
+
+}
