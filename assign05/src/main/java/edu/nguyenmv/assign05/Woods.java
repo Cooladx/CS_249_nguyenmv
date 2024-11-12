be6ea24331b78e6b28400991ad09877272a778a2
Mike Nguyen <mikeynguyen29@gmail.com>
2024-11-12 15:35:32 -0500

    fixing exercises.

---
+++ assign05/src/main/java/edu/nguyenmv/assign05/Woods.java
@@ -0,0 +1,13 @@
+package edu.nguyenmv.assign05;
+import java.util.*;
+public class Woods {
+    ArrayList<String> Giants = new ArrayList<>();
+    public Giant createGiant (String name, String typeName) {
+        if (name.isEmpty()) {
+            return null;
+        }
+        Giants.add("name");
+        System.out.println(Giants);
+
+    }
+}
---
+++ exercises/src/main/java/edu/nguyenmv/exercises13/InvalidRadiusException.java
@@ -0,0 +1,15 @@
+package edu.nguyenmv.exercises13;
+
+public class InvalidRadiusException extends Exception{
+public InvalidRadiusException() {
+    super();
+}
+public InvalidRadiusException(String message) {
+    super(message);
+}
+public InvalidRadiusException(String message, Exception e) {
+super(message, e);
+        }
+
+
+}
